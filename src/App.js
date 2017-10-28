c31dd4ba088a25228a4efff2ea83ce2d9a3bd4cd
John <pwnerjohn@gmail.com>
2017-10-28 06:06:53 -0400

    learning how to web dev is kind of hard!

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+
+class UploadFiles extends React.Component {
+    upload = () => {
+        console.log("uploading...");
+        var uploaded = document.getElementById("filesToUpload");
+        var uploadstatus = document.getElementById("uploadStatus");
+        uploadstatus.innerHTML = "";
+     
+        if (uploaded.files.length > 1) {
+            console.log("Error: This application currently supports the uploading of only one file at a time.");
+            uploadstatus.innerHTML = "Error: This application currently supports the uploading of only one file at a time.";
+            return;
+        }
+
+        var image = uploaded.files[0];
+
+        if (!image.type.match('image.*')) {
+            console.log("Error: This file is of an unsupported format... is it an image?");
+            uploadstatus.innerHTML = "Error: This file is of an unsupported format... is it an image?"
+            return;
+        }
+
+        console.log("image passes all test cases!");
+
+        // probably do some kind of POST to upload api...
+
+    }
+    
+    render() {
+        return(
+            <div className="toUpload">
+                <input type="file" id="filesToUpload" />
+                <button onClick={this.upload}>Upload</button>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="header">
+                    <h1>Upload Image(s?) Here</h1>
+                </div>
+
+
+                <div className="body">
+                    <div id="uploadStatus">
+                    </div>
+                    <div className="imageupload">
+                        <UploadFiles />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
